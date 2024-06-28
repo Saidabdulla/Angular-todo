0cb59aac0f6c01ca9e7757caa7e1153737b20889
s_masharipov <abdullamasharipov11@gmail.com>
2024-06-28 22:40:52 +0500

    Login page

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
-
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { AuthService } from "../../servies/auth.service";
+import { LoginCredentials } from "../../interfaces/auth";
+import { TooltipModule } from 'primeng/tooltip';
 
 
 @Component({
@@ -10,10 +13,40 @@ import { ButtonModule } from 'primeng/button';
   styleUrls: ['./login.component.scss'],
   standalone: true,
   imports: [
+    ReactiveFormsModule,
     InputTextModule,
+    TooltipModule,
     ButtonModule
   ]
 })
 export class LoginComponent {
+  authService = inject(AuthService);
+
+  emailErrorText = '';
+  passwordErrorText = '';
+
+  form = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")]),
+    password: new FormControl('', [Validators.required])
+  });
+
+  submit() {
+    if (this.form.invalid) {
+      if (!this.form.get('email')?.value) {
+        this.emailErrorText = 'Email is required!';
+      } else {
+        this.emailErrorText = 'Email doesn\'t match E-mail pattern!'
+      }
+
+      if (!this.form.get('password')?.value) {
+        this.passwordErrorText = 'Password is required!';
+      }
+
+    } else {
+      this.authService.login(this.form.value as LoginCredentials);
+    }
+  }
 
 }
