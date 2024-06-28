062705a28485604cf79341c45c2ec329eebed0e2
s_masharipov <abdullamasharipov11@gmail.com>
2024-06-29 00:29:39 +0500

    Todo list

--- src/app/modules/main/components/list/list.component.ts
+++ src/app/modules/main/components/list/list.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from "@angular/common/http";
 import { Todo } from "../../../../interfaces/todos";
 import { environment } from "../../../../../environments/environment";
 import { CommonModule } from "@angular/common";
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-list',
@@ -11,7 +13,7 @@ import { CommonModule } from "@angular/common";
   standalone: true,
   imports: [CommonModule]
 })
-export class ListComponent implements OnInit{
+export class ListComponent implements OnInit {
   http = inject(HttpClient);
 
   todos: Todo[] = [];
@@ -19,9 +21,22 @@ export class ListComponent implements OnInit{
   fetchTodos(): void {
     const url = `${environment.apiUrl}todo/`;
 
-    this.http.get(url).subscribe((res: any) => {
-      this.todos = res.results;
-    })
+    this.http.get(url).pipe(
+      map((res: any) => {
+        // Sort array with completed: false first, completed: true last
+        return res.results.sort((a: Todo, b: Todo) => {
+          if (a.completed === b.completed) {
+            return 0;
+          } else if (a.completed) {
+            return 1;
+          } else {
+            return -1;
+          }
+        });
+      })
+    ).subscribe((sortedArray: Todo[]) => {
+      this.todos = sortedArray
+    });
   }
 
   ngOnInit(): void {
